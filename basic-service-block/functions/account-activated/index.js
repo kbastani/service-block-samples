e7f24b17cb8e9e024252566fecba8ef8a11e908e
kbastani <kb@socialmoon.com>
2017-05-08 14:00:24 -0700

    Create MongoDB connection using CF and Lambda

--- basic-service-block/functions/account-activated/index.js
+++ basic-service-block/functions/account-activated/index.js
@@ -1,4 +1,9 @@
 exports.handler = (event, context, callback) => {
+
+    var result = store(function(db) {
+        return db.collection('inserts').insertOne(event);
+    });
+
     var events = event.eventLog;
     var account = event.account;
 
@@ -16,3 +21,32 @@ exports.handler = (event, context, callback) => {
         callback(error);
     }
 };
+
+
+function store(transaction) {
+
+    // Fetch connection from environment
+    var url = process.env.SERVICE_CREDENTIALS.uri;
+
+    // Create mongo client
+    var mongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+    // Create transaction context
+    var apply = function(transaction) {
+        var response = {};
+
+        // Use connect method to connect to the server
+        mongoClient.connect(url, function (err, db) {
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+
+            // Apply transaction
+            response = transaction(mongoClient);
+            db.close();
+        });
+
+        return response;
+    };
+
+    return apply(transaction);
+}
