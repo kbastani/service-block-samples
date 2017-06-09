00e883c927cc368ac5228dcf79c756b4ffaf0bda
kbastani <kb@socialmoon.com>
2017-06-09 06:28:58 -0700

    Fixing function tests

--- cqrs-block/functions/project-created/src/test/java/com/example/demo/DemoApplicationTests.java
+++ cqrs-block/functions/project-created/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import demo.functions.ProjectCreatedFunction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ProjectCreatedFunction.class)
 public class DemoApplicationTests {
 
 	@Test
