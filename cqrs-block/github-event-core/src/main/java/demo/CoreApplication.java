e1fff8e236e7d82f1499db88381811e474d23454
kbastani <kb@socialmoon.com>
2017-06-13 11:19:28 -0700

    Exclude database initializer on test

--- cqrs-block/github-event-core/src/main/java/demo/CoreApplication.java
+++ cqrs-block/github-event-core/src/main/java/demo/CoreApplication.java
@@ -1,13 +1,9 @@
 package demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.CollectionOptions;
-import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
@@ -18,18 +14,6 @@ import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType
 public class CoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoreApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(MongoOperations operations) {
-        return (args) -> {
-            // Setup the streaming data endpoint
-            if (operations.collectionExists("tightCouplingEvent")) {
-                operations.dropCollection("tightCouplingEvent");
-            }
-            CollectionOptions options = new CollectionOptions(5242880, 5000, true);
-            operations.createCollection("tightCouplingEvent", options);
-        };
     }
 }
 
--- cqrs-block/github-event-core/src/main/java/demo/config/MongoConfig.java
+++ cqrs-block/github-event-core/src/main/java/demo/config/MongoConfig.java
@@ -1,18 +1,35 @@
 package demo.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.CollectionOptions;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableReactiveMongoRepositories
 @EnableMongoAuditing
+@Profile({"development", "cloud"})
 public class MongoConfig extends AbstractCloudConfig {
 
     @Bean
     public MongoDbFactory mongoFactory() {
         return connectionFactory().mongoDbFactory();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(MongoOperations operations) {
+        return (args) -> {
+            // Setup the streaming data endpoint
+            if (operations.collectionExists("tightCouplingEvent")) {
+                operations.dropCollection("tightCouplingEvent");
+            }
+            CollectionOptions options = new CollectionOptions(5242880, 5000, true);
+            operations.createCollection("tightCouplingEvent", options);
+        };
     }
 
 }
